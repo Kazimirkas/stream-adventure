f730418148677eab8cd71f0650f40af6ce8430a4
Kazimirkas <vyperdos@gmail.com>
2016-12-21 14:25:25 +0200

    concat done

---
+++ concat.js
@@ -0,0 +1,6 @@
+const concat = require('concat-stream');
+
+process.stdin.pipe(concat(function (buffer) {
+  process.stdout.write(buffer.toString().split('').reverse().join(''))
+  process.stdout.write('\n')
+}));
